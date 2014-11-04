71af5b39e80d5e10ccb5f56cc1c4dba89f7ecb60
Robert Yang <yang.robert.w@gmail.com>
2014-11-04 16:52:25 -0500

    week 10: physics input demo, basic space ship controls

---
+++ week10/Assets/scripts/PhysicsInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsInput : MonoBehaviour {
+
+	public float speed = 5f; // remember, "public" exposes a var in the inspector
+
+	// FixedUpdate is called on a fixed time interval; Physics runs at this framerate
+	void FixedUpdate () {
+
+		// is "W" being held down on the keyboard?
+		if ( Input.GetKey (KeyCode.W) ) {
+			// transform.up automatically calculates the object's "local up" in global worldspace
+			GetComponent<Rigidbody>().AddForce( transform.up * speed );
+		}
+
+		// turn left code
+		if ( Input.GetKey (KeyCode.A) ) {
+			GetComponent<Rigidbody>().AddTorque( 0f, 0f, 1f );
+		}
+
+		// turn right code
+		if ( Input.GetKey ( KeyCode.D) ) {
+			GetComponent<Rigidbody>().AddTorque( 0f, 0f, -1f );
+		}
+
+	}
+
+}
