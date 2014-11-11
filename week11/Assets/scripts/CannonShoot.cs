58c562d0226b58bfab57dfcac36408a10c968dab
Robert Yang <yang.robert.w@gmail.com>
2014-11-11 15:00:09 -0500

    added syllabus + week 11 code (instantiation, screen shake)

---
+++ week11/Assets/scripts/CannonShoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// place this on our cube, to make it shoot cannonballs
+public class CannonShoot : MonoBehaviour {
+
+	// to tell Unity what we want to make clones of, we need an original object to clone!
+	public Transform originalCannonball; // assign in Inspector
+
+	// Use this for initialization
+	void Start () {
+		// test out instantiation
+		Instantiate( originalCannonball, transform.position, Quaternion.Euler(0f, 90f, 0f) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if I press spacebar, spawn a cannonball
+		if ( Input.GetKeyDown (KeyCode.Space) ) {
+			Instantiate( originalCannonball, transform.position + transform.forward, Quaternion.Euler(0f, 90f, 0f) );
+		}
+	}
+}
---
+++ week11/Assets/scripts/ScreenShake.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// place this on a cannonball object (or, any object we want to clone!)
+public class ScreenShake : MonoBehaviour {
+
+	// each cannonball clone will start with 1.0 (100%) shakeStrength
+	float shakeStrength = 1f;
+
+	// Update is called once per frame
+	void Update () {
+		// check to see if the ball is close enough to y=0, which means it hit the ground
+		if (transform.position.y < 0.5f) {
+			// note that this is an unstable implementation... after enough shakes,
+			// the camera will eventually drift (because it doesn't remember where it started)
+			Camera.main.transform.position += Camera.main.transform.right * 
+												Mathf.Sin (Time.time * 50f) * 
+												0.1f * shakeStrength;
+
+			// after 1 second, shakeStrength will go from 1 to 0
+			shakeStrength -= Time.deltaTime;
+
+			// Clamp functions limit values to a certain range... here, between 0 and 1
+			shakeStrength = Mathf.Clamp ( shakeStrength, 0f, 1f );
+		} // end if statement
+		
+	} // end update
+} // end class
